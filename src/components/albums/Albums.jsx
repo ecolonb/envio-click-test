e3cfb3595eda98b4d03da8dc66f6d08592ac67c6
Edd Colbar <edgar.colonb@gmail.com>
2019-04-14 22:10:08 -0500

    Se modificó el componente Albums, se muestra el listado de usuarios (en este ejemplo utilizo AXIOS con asyn – await, el prozimo request será con proesas resolve,reject - then -> catch ), se trabajará en visualizar los albums del usuario en otro componente pasando el id por router.

--- src/components/albums/Albums.jsx
+++ src/components/albums/Albums.jsx
@@ -1,11 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Table } from 'react-bootstrap';
+
+//Servicios
+import { getUsersWithAlbums } from '../../services/users';
+
 import './albums.scss';
 export default function() {
+  const [users, setUsers] = useState(undefined);
+
+  useEffect(() => {
+    console.log('On use effect-->>>');
+    loadUSers();
+  }, []);
+  async function loadUSers() {
+    console.log('Obteniendo los usuarios');
+    const usersWA = await getUsersWithAlbums();
+    setUsers(usersWA.data);
+    console.log('usersWA: ', usersWA);
+  }
+
   return (
     <div className="albums-container">
-      <div className="albums-user">
-        <p>albums user container</p>
-        lorem1000
+      <div className="list-users">
+        <Table
+          responsive
+          className="table table-striped"
+          style={{ zIndex: '0' }}
+        >
+          <thead className="bg-secondary" style={{ color: '#FFFFFF' }}>
+            <tr>
+              <th>Name</th>
+              <th>Username</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Website</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users &&
+              users.map((item, index) => {
+                return item === undefined ? (
+                  false
+                ) : (
+                  <tr
+                    className="item-user"
+                    onClick={e => {
+                      // showDetails(e);
+                      console.log('View photos id: ', item.id);
+                    }}
+                  >
+                    <td>{item.name}</td>
+                    <td>{item.username}</td>
+                    <td>{item.email}</td>
+                    <td>{item.phone}</td>
+                    <td>{item.website}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </Table>
       </div>
     </div>
   );
--- src/services/users.js
+++ src/services/users.js
@@ -1,6 +1,15 @@
+import axios from 'axios';
+
 export default async function getUsersByPage(page) {
     const url = `https://reqres.in/api/users?page=${page}&delay=4`;
     const response = await fetch(url);
     const json = await response.json();
     return json;
+}
+
+export async function getUsersWithAlbums() {
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    const usersWithAlbum = await axios.get(url);
+    console.log('usersWithAlbum: ', usersWithAlbum);
+    return usersWithAlbum;
 }
